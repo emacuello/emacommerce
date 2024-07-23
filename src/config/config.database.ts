4ee6822b3a21cd76e2a854657ee15f3e2ef90440
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-07-23 18:30:03 -0300

    migrando a vercel

--- src/config/config.database.ts
+++ src/config/config.database.ts
@@ -5,12 +5,7 @@ dotenvConfig({ path: '.env.development' });
 
 const configDatabase = {
     type: 'postgres',
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    // host: 'localhost',
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    url: process.env.DB_URL,
     entities: ['dist/**/*.entity{.ts,.js}'],
     // synchronize: true,
     logging: true,
